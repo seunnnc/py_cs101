b4576a2d5d98104149042658bbd9e7852a900e44
TwoSO <seunnnc@users.noreply.github.com>
2021-03-07 16:51:44 +0900

    for, while 기초

--- forAndWhile.py
+++ forAndWhile.py
@@ -5,6 +5,7 @@ create_world()
 hubo = Robot()
 sleep_time = 0.5
 
+# 움직이고 beeper 줍기
 def move_and_pick():
     time.sleep(sleep_time)
     hubo.move()
@@ -12,5 +13,6 @@ def move_and_pick():
         time.sleep(sleep_time)
         hubo.pick_beeper()
 
+# 9번 반복
 for i in range(9):
     move_and_pick()
