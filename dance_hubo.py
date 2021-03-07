c440f13caf32582f03e405b1945ef146b461c71a
TwoSO <seunnnc@users.noreply.github.com>
2021-03-07 16:51:11 +0900

    춤추는 휴보

--- dance_hubo.py
+++ dance_hubo.py
@@ -5,11 +5,13 @@ create_world(avenues = 3, streets = 3)
 hubo = Robot(beepers = 10)
 sleep_time = 0.5
 
+# 춤추기!
 def dance():
     for i  in range(4):
         time.sleep(sleep_time)
         hubo.turn_left()
 
+# 한칸 움직이고 돌기
 def move_or_turn():
     if hubo.front_is_clear():
         dance()
@@ -21,5 +23,6 @@ def move_or_turn():
         time.sleep(sleep_time)
         hubo.drop_beeper()
 
+# move or turn 18번 반복
 for i in range(18):
     move_or_turn()
